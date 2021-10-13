b04484f76b190666384a76cd5fe3ca3b15944bd8
Tony Rozzi <rozzit@vcu.edu>
2021-10-13 16:26:01 -0400

    Added TarotCard class.

---
+++ Zephyrus/app/src/main/java/com/example/zephyrus/TarotCard.java
@@ -0,0 +1,45 @@
+package com.example.zephyrus;
+
+import android.graphics.drawable.Drawable;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TarotCard {
+    private Drawable tarotCardImage;
+    private ArrayList<String> captions; // the things that go in the bubbles below the card, idk what they're called
+    private String description;
+
+    public TarotCard(File imageFile, ArrayList<String> captions, String description)
+    {
+        if(description == null)
+            throw new IllegalArgumentException("description cannot be null");
+        if(captions == null)
+            throw new IllegalArgumentException("captions cannot be null");
+        if(captions.size() == 0)
+            throw new IllegalArgumentException("The number of captions must be > 0");
+        if(imageFile == null)
+            throw new IllegalArgumentException("imageFile cannot be null");
+        if(!imageFile.exists())
+            throw new IllegalArgumentException("imageFile '" + imageFile.getAbsolutePath() + "' does not exist");
+
+        this.captions = captions;
+        this.description = description;
+        this.tarotCardImage = Drawable.createFromPath(imageFile.getAbsolutePath());
+    }
+
+    public Drawable getImage()
+    {
+        return this.tarotCardImage;
+    }
+
+    public ArrayList<String> getCaptions()
+    {
+        return this.captions;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+}
