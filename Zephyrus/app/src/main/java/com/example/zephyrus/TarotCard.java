9c09a69c2b838b9a691a227cc3723d19eaaf929f
Jcheng8293 <chengj@vcu.edu>
2021-10-15 14:47:38 -0400

    Fixed transition in DailyTarot (renamed to CardFacts to make it clearer on what it does) Added shuffle() and reverse() methods to TarotCards

--- Zephyrus/app/src/main/java/com/example/zephyrus/TarotCard.java
+++ Zephyrus/app/src/main/java/com/example/zephyrus/TarotCard.java
@@ -11,8 +11,9 @@ public class TarotCard implements Serializable
     private final Drawable tarotCardImage;
     private final String[] captions; // the things that go in the bubbles below the card, idk what they're called
     private final String description;
+    private int state; // 0 is regular, 1 is reverse
 
-    public TarotCard(File imageFile, String[] captions, String description)
+    public TarotCard(File imageFile, String[] captions, String description, int state)
     {
         if(description == null)
             throw new IllegalArgumentException("description cannot be null");
@@ -25,6 +26,7 @@ public class TarotCard implements Serializable
         if(!imageFile.exists())
             throw new IllegalArgumentException("imageFile '" + imageFile.getAbsolutePath() + "' does not exist");
 
+        this.state = 0;
         this.captions = captions;
         this.description = description;
         this.tarotCardImage = Drawable.createFromPath(imageFile.getAbsolutePath());
@@ -43,5 +45,43 @@ public class TarotCard implements Serializable
     public String getDescription()
     {
         return this.description;
+    }
+
+    public int getState() { return this.state; }
+
+    public void setState(int state) { this.state = state; }
+
+
+    public ArrayList<TarotCard> shuffle(ArrayList<TarotCard> deck, ArrayList<TarotCard> regularDeck, ArrayList<TarotCard> reverseDeck) {
+        ArrayList<TarotCard> temp = new ArrayList<>();
+
+        for (int i = 38; i <= deck.size(); i++) {
+            temp.add(deck.remove(i)); }
+
+        int random = (int) Math.round(Math.random());
+
+        if (random == 0) { reverse(deck); }
+        else { reverse(temp); }
+
+        ArrayList<TarotCard> newDeck = new ArrayList<>();
+
+        for (int i = 0; deck.size() != 0 && temp.size() != 0; i++) {
+            newDeck.add(deck.remove(i));
+            newDeck.add(temp.remove(i));
+        }
+
+        return newDeck;
+    }
+
+    public void reverse(ArrayList<TarotCard> deck) {
+
+        for (int i = 0; i <= deck.size(); i++) {
+            if (deck.get(i).getState() == 0) {
+                deck.get(i).setState(1);
+            }
+            else {
+                deck.get(i).setState(0);
+            }
+        }
     }
 }
